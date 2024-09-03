2b860c19365e1db0ef81c545c0634b4d93582edd
ramesh331318 <ramesh33138@gmail.com>
2024-09-03 19:51:09 +0530

    Singleton design pattern has created

---
+++ singleton_design_pattern/Singleton.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+//In this pattern we can create more than one Instance/obeject of the class
+//All methods and variables should be in static
+//constructor and destructor should be in private 
+//It doesn't use more RAM memory because we have created only one instance
+//Usually we can't create object for constructor from outside the class if we make it as a private
+//As I mentioned all methods are static so it doesn't use heap area. it always created in stack area only
+class Singleton{
+	public:
+		static Singleton* create_instance() {
+			if(m_pInstance == nullptr) {
+				m_pInstance = new Singleton();
+			}
+			return m_pInstance;
+		}
+		static void destroy () {
+			if(m_pInstance != nullptr) {
+				delete m_pInstance;
+				m_pInstance = nullptr;
+			}
+		}
+	private:
+	        Singleton(){
+			std::cout<<"constructor created\n";
+		}
+		 ~Singleton () {
+			std::cout<<"destructor destroys the object"<<std::endl;
+		}
+		static Singleton* m_pInstance;
+
+
+};
+Singleton* Singleton::m_pInstance = nullptr;
+int main () {
+	Singleton::create_instance();
+	Singleton::destroy();
+	return 0;
+}
